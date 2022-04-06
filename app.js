434958c718644791f33f49b5353f1e464c4e1943
Tom Radford <tom@theradford.com>
2022-04-06 22:32:10 +0200

    complete up to 4,7

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const config = require('./utils/config');
-const logger = require('./utils/logger');
-const blogsRouter = require('./controllers/blogs');
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const blogsRouter = require('./controllers/blogs')
 
-const app = express();
+const app = express()
 
 mongoose.connect(config.MONGODB_URI)
   .then(() => logger.info('Connected to MongoDB'))
-  .catch((error) => logger.error('error connecting to MongoDB', error.message));
+  .catch((error) => logger.error('error connecting to MongoDB', error.message))
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
-app.use('/api/blogs', blogsRouter);
+app.use('/api/blogs', blogsRouter)
 
-module.exports = app;
+module.exports = app
