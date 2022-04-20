f1ba737688a888fbaaa2a9de0b84d55f84df133f
Tom Radford <tom@theradford.com>
2022-04-20 16:27:42 +0200

    up to excercise 4.14

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,22 +1,38 @@
-const blogsRouter = require('express').Router();
-const Blog = require('../models/blog');
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-  Blog
-    .find({})
-    .then((blogs) => {
-      response.json(blogs);
-    });
-});
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
+})
 
-blogsRouter.post('/', (request, response) => {
-  const blog = new Blog(request.body);
+blogsRouter.post('/', async (request, response) => {
+  if (!request.body.title) {
+    return response.status(400).end()
+  }
+  if (!request.body.url) {
+    return response.status(400).end()
+  }
 
-  blog
-    .save()
-    .then((result) => {
-      response.status(201).json(result);
-    });
-});
+  const newBlog = request.body
+  newBlog.likes = newBlog.likes
+    ? newBlog.likes
+    : 0
+  const blog = new Blog(newBlog)
+  const savedNote = await blog.save()
+  response.status(201).json(savedNote)
+})
 
-module.exports = blogsRouter;
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const { body } = request
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, body, { new: true } )
+  response.json(updatedBlog)
+})
+
+
+module.exports = blogsRouter
